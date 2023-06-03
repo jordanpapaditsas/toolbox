d919b77bb2a5fd3503fe37f0d31216d92af5bc11
Iordanis Papaditsas <114758586+jordanpapaditsas@users.noreply.github.com>
2023-06-03 15:00:23 +0300

    Initial commit

---
+++ JavaScript/counter/index.js
@@ -0,0 +1,20 @@
+let count = 0;
+
+document.getElementById("decreaseBtn").onclick = function() {
+  count = count - 1;
+  document.getElementById("countLabel").innerHTML = count;
+
+}
+
+document.getElementById("resetBtn").onclick = function() {
+  count = 0;
+  document.getElementById("countLabel").innerHTML = count;
+
+  
+}
+
+document.getElementById("increaseBtn").onclick = function() {
+  count = count + 1;
+  document.getElementById("countLabel").innerHTML = count;
+  
+}
