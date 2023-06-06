5318407282975abca9720df26868c5afd618fe5c
Iordanis Papaditsas <114758586+jordanpapaditsas@users.noreply.github.com>
2023-06-06 22:22:46 +0300

    Undefined examples

---
+++ JavaScript/undefined/index.js
@@ -0,0 +1,15 @@
+let x;
+let y;
+let z;
+let counter = 0;
+
+// Giving undefined (No initialization in variables 'x', 'y', 'z')
+for (i = 1; i < counter; i++) {
+  x++; 
+  y++;
+  z++;
+}
+
+console.log(x, y, z);
+
+
